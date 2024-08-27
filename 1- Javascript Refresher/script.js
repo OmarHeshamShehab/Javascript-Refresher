ca0f356ae153454af9bc376d5554f777f902a594
unknown <omar.hesham.shehab@gmail.com>
2024-08-27 23:51:51 +0300

    added

--- 1- Javascript Refresher/script.js
+++ 1- Javascript Refresher/script.js
@@ -1,4 +1,24 @@
-let a = 'Omar';
-let b = 'Hesham';
+// Template literals
 
-console.log(`${a} ${b}`)
+let a = "Omar";
+let b = "Hesham";
+
+console.log(`${a} ${b}`);
+
+// Ternary Operations
+
+let food1 = "pizza";
+let food2 = "burger";
+
+const foodStatus = false;
+
+function getFood1(foodName) {
+  console.log(food1);
+}
+function getFood2(foodName) {
+  console.log(food2);
+}
+
+foodStatus ? getFood1() : getFood2();
+
+// Object and array destructuring
